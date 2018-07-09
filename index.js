af1df14501263f9a4cc7c543f978d14249e71d6c
Akash-sh <hereisakash01@gmail.com>
2018-07-09 13:23:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const TOKEN = '611698842:AAHcE4x2vl1v0ygvtLKqnkq1arbsgAhvlkg'
 
 const bot = new TelegramBot(TOKEN, {polling: true})
 
-bot.on('message', msg => {
-    if (msg.new_chat_members != undefined) {
-    bot.sendMessage(message.chat.id, message.new_chat_member.username + " joined!"); }
+bot.on('message', function (message) {
+    if (message.new_chat_members != undefined) {
+        bot.sendMessage(message.chat.id, message.new_chat_member.first_name + " joined!");
+    }else{
+        console.log("new_chat_members is not defined");
+    }
 });
